fed0db84639d1c56cbf6b437510e022503117b1e
Darya Makeeva <dasha.makeeva.99@mail.ru>
2023-08-17 12:16:49 +0300

    first_commit

---
+++ js/submit.js
@@ -0,0 +1,49 @@
+var username = document.getElementById('username');
+var email = document.getElementById('email');
+var submit = document.getElementById('send');
+var messages = document.getElementById('formMsg')
+
+submit.addEventListener('click', (e) => {
+    
+    e.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = () => {
+        let responseObject = null;
+
+        try {
+            responseObject = JSON.parse(xhr.responseText);
+        } catch (e) {
+            console.error('Could not parse JSON!');
+        }
+
+        if (responseObject) {
+            handleResponse(responseObject);
+        }
+    };
+
+    const xhrData = `username=${username.value}&password=${password.value}`;
+
+    xhr.open('POST', '/php/submit.php');
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(xhrData);
+});
+
+function handleResponse (responseObject) {
+    if (responseObject.ok) {
+        location.href = '/html/submit.html';
+    } else {
+        while (messages.firstChild) {
+            messages.removeChild(messages.firstChild);
+        }
+
+        responseObject.messages.forEach((message) => {
+            const li = document.createElement('li');
+            li.textContent = message;
+            messages.appendChild(li);
+        });
+
+        messages.style.display = "block";
+    }
+}
---
+++ php/submit.php
@@ -0,0 +1,33 @@
+<?php
+
+    $username   = isset($_POST['username']) ? $_POST['username'] : '';
+    $email      = isset($_POST['email']) ? $_POST['email'] : '';
+
+    $ok = true;
+    $messages = array();
+
+    if ( !isset($username) || empty($username) ) {
+        $ok = false;
+        $messages[] = 'Username cannot be empty!';
+    }
+
+    if ( !isset($email) || empty($email) ) {
+        $ok = false;
+        $messages[] = 'Email cannot be empty!';
+    }
+
+    if ($ok) {
+        if ($username === 'dcode' && $email === 'dcode@mail.ru') {
+            $ok = true;
+            $messages[] = 'Successful reserve!';
+        } 
+    }
+
+    echo json_encode(
+        array(
+            'ok' => $ok,
+            'messages' => $messages
+        )
+    );
+
+?>
